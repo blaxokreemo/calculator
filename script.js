d72f97d03307afa2d89a80b1e84ab60192585933
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-04-17 08:46:40 -0400

    Create operate function

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function divide(x, y) {
     return x / y;
 }
 
+let leftNum = 0;
+let rightNum = 0;
+let operator = "";
+
+function operate(x, oper, y) {
+    switch (oper) {
+        case "+":
+            return add(x, y);
+            break;
+        case "-":
+            return substract(x, y);
+            break;
+        case "*":
+            return multiply(x, y);
+            break;
+        case "/":
+            return divide(x, y);
+            break;
+        default:
+            break;
+    }
+}
